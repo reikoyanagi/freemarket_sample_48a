6bb07df53c77ba6d7b528334e8a4afc5405a4f65
smoto-shei <smoto_shei@yahoo.co.jp>
2019-04-15 17:19:32 +0900

    Made files for uploading images
    出品機能を作るために、
    ・Images テーブルの作成
    ・Image モデルの作成
    ・Uploader の作成
    を行いました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_035517) do
+ActiveRecord::Schema.define(version: 2019_04_15_074432) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id"
+    t.string "item_image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
