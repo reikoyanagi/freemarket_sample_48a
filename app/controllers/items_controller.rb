a427c28f98df46d620c1dfa2534fd3a18740dcd7
smoto-shei <smoto_shei@yahoo.co.jp>
2019-04-18 17:19:41 +0900

    [WIP] making sell items function
    Items, images, deliveries 各テーブルにデータが保存できるようにしました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,19 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    # 出品機能
     @item = Item.new
     @item.images.build
     @item.build_delivery
   end
 
   def create
-    binding.pry
+    # 出品機能
     Item.create(item_params)
   end
 
   private
 
   def item_params
+    # 出品機能
     params.require(:item).permit(:name, :user_id, :condition, :price, :detail, :status_id, :brand, :size, images_attributes: [:item_id, :item_image], delivery_attributes: [:postage, :shipping, :region, :shipping_date])
   end
 end
