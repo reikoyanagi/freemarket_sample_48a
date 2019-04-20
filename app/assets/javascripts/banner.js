986191b5c016a7c2bc71a5331e8d12c50255428a
reikoyanagi <pxfpd899>
2019-04-20 10:06:03 +0900

    slick使用実装

---
+++ app/assets/javascripts/banner.js
@@ -0,0 +1,9 @@
+$(function(){
+  $(".slider").slick({
+ autoplay: true,
+  prevArrow: '<a class="slick-prev" href="#">＜</a>',
+nextArrow: '<a class="slick-next" href="#">＞</a>',
+dots: true,
+
+  });
+});
