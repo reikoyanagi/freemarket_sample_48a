eb8b774f294e8750655d9ca5b7e5fcc2764b15ae
isoppu1632 <owner@MA4-0026.local>
2019-04-21 19:10:32 +0900

    fix select
    ・セレクトタグの未入力制限の微調整

--- app/assets/javascripts/birthday_select.js
+++ app/assets/javascripts/birthday_select.js
@@ -6,19 +6,19 @@ $(function() {
   var year_val = current.getFullYear();
   // プルダウン生成
   //年
-  $('#year_pulldown').html('<select name="session[birth_year]"><option selected>---</opution>');
+  $('#year_pulldown').html('<select required name="session[birth_year]" required ><option value="">---</opution>');
   for (var i = year_val ; i > 1989 ;  i--) {
     $('#year_pulldown select').append('<option value="' + i + '">' + i + '</option>');
   }
 
   //月
-  $('#month_pulldown').html('<select name="session[birth_month]"><option selected>--</opution>');
+  $('#month_pulldown').html('<select name="session[birth_month]" required ><option value="">--</opution>');
   for (var i = 1; i <= 12; i++) {
     $('#month_pulldown select').append('<option value="' + i + '">' + i + '</option>');
   }
 
   //日
-  $('#day_pulldown').html('<select name="session[birth_day]"><option selected>--</opution>');
+  $('#day_pulldown').html('<select name="session[birth_day]" required ><option value="">--</opution>');
   for (var i = 1; i <= 31; i++) {
     $('#day_pulldown select').append('<option value="' + i + '">' + i + '</option>');
   }
