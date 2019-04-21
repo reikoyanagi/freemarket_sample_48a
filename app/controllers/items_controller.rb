51d08257f753806fd4894285de3f0be5389ca05a
reikoyanagi <pxfpd899>
2019-04-21 19:01:33 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,15 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+      item.destroy
+    redirect_to controller: :items, action: :index
+    end
   end
 
   private
