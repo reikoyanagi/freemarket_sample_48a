f94bff6208e4c6a6bd980d5bd76d42459c8284a2
SHOTA1107 <s.kobayashi11788@gmail.com>
2019-04-21 13:19:03 +0900

    transaction-controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,16 @@
 class TransactionsController < ApplicationController
 
-  def show
+  def new
+    @item = Item.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def create
   end
 
   private
+  def item_params
+    params.require.(:item)
+  end
 
 end
