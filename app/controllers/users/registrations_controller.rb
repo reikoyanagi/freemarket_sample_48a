b361d4a3c34da902c34ef353ea0e6e8ece5b25f5
isoppu1632 <owner@MA4-0026.local>
2019-04-21 21:03:01 +0900

    fix routes.rb
    ＃WHAT
    ・deviseで出来ることはdeviseで記入
    ・記入修正
    
    #WHY

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,9 +4,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  def signin
-
-  end
 
   def registration
 
@@ -137,11 +134,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    signup_done_path
+    sign_up_done_path
   end
 
   # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
-    signup_done_path
+    sign_up_done_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    :sessions           => "users/sessions",
-    :registrations      => "users/registrations",
-    :passwords          => "users/passwords",
-  },
-  skip: [:sessions,:registrations]
+    :registrations      => "users/registrations"
+  }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
 
   as :user do
-    get 'signin' => 'devise/sessions#new', as: :new_user_session
-    post 'signin' => 'devise/sessions#create', as: :user_session
-    match 'signout' => 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
-    get 'signout' => 'devise/sessions#destroy'
 
-    get 'signup' => 'users/registrations#signup'#新規会員登録
-    get "/signup/registration" => "users/registrations#registration"#会員情報入力
-    post "/signup/tel" => 'users/registrations#tel'#電話番号登録
-    post "/signup/address" => 'users/registrations#address'#住所確認
-    post "/signup/credit" => 'users/registrations#credit'#クレジットカード
-    post "/signup/completed" => 'users/registrations#create'#DBに保存
-    get "/signup/done" => 'users/registrations#done'#完了ページ
+    get "/sign_up/registration" => "users/registrations#registration"#会員情報入力
+    post "/sign_up/tel" => 'users/registrations#tel'#電話番号登録
+    post "/sign_up/address" => 'users/registrations#address'#住所確認
+    post "/sign_up/credit" => 'users/registrations#credit'#クレジットカード
+    post "/sign_up/create" => 'users/registrations#create'#DBに保存
+    get "/sign_up/done" => 'users/registrations#done'#完了ページ
   end
 
   resources :items
   resources :users
-    resources :credit_cards
-    resources :transactions
+  resources :credit_cards
+  resources :transactions
 end
