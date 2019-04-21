a928b836a429de8908ac685fcf869af9741980f6
isoppu1632 <owner@MA4-0026.local>
2019-04-21 18:50:13 +0900

    Fix link sign_in & sign_up & sign_out
    ・メインメニューからsign_in sign_up sign_outの一連の流れを作成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  def signin
+
+  end
+
   def registration
+
   end
 
   def tel
