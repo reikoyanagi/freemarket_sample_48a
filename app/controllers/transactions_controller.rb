c286a507dec626911c43c77d531b81d81ed5806f
SHOTA1107 <s.kobayashi11788@gmail.com>
2019-04-23 12:44:11 +0900

    edit-transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,20 @@
 class TransactionsController < ApplicationController
 
+  require 'payjp'
+
   def new
     @item = Item.find(params[:item_id])
     @transaction = Transaction.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    # Payjp.api_key = 'sk_test_0ed9e660871befcb2421e447'
+    # charge = Payjp::Charge.create(
+    #   :amoumt => @item.price,
+    #   :card => "4242424242424242",
+    #   :currency => 'jpy',
+    #   )
   end
 
   private
