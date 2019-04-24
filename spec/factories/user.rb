8349745cecd5102bdbe49b8d2d3c3c732395211b
isoppu1632 <owner@MA4-0026.local>
2019-04-24 15:58:49 +0900

    fix Faker use
    ・Fakerを使って修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
 
   factory :user do
     nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
-    last_name             {"田中"}
-    first_name            {"太郎"}
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password }
+    password_confirmation { Faker::Internet.password }
+    last_name             { Faker::Name.last_name }
+    first_name            { Faker::Name.first_name }
     last_name_kana        {"タナカ"}
     first_name_kana       {"タロウ"}
     birth_date            {"2000-12-30"}
