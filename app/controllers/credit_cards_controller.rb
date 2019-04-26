adcc935b1c1b049259a8ea7d1bcf7b176a7ba732
SHOTA1107 <s.kobayashi11788@gmail.com>
2019-04-26 18:33:54 +0900

    credit-model-controller
    モデルの修正、コントローラーの定義

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,16 +1,42 @@
 class CreditCardsController < ApplicationController
 
-
+  before_action :authenticate_user!
   require "payjp"
 
   def index
+    @credit_card = Credit_card.new
   end
 
   def show
+    @credit_card = Credit_card_where(user_id: current_user.id).first
+    if @credit_card.blank?
+      redirect_to action: "new"
+    end
+  end
+
+  def new
+    Payjp.api_key = 'sk_test_0ed9e660871befcb2421e447'
+    Payjp::Token.create({
+      :card => {
+        :number => '4242424242424242',
+        :exp_month => '2',
+        :exp_year => '2020',
+        :cvc => '123'
+      }},
+      {
+        'X-Payjp-Direct-Token-Generate': 'true'
+      })
   end
 
 
   def create
+    @credit_card = Credit_card.new(credit_params)
+    @credit_card.save
+  end
+
+  private
+  def credit_params
+    params.require(:credit_card).permit(:token_id)
   end
 
 end
