23d96c9214fcaa1b9bae2abe0fe97fea18ed88c0
SHOTA1107 <s.kobayashi11788@gmail.com>
2019-04-27 19:38:26 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_044836) do
+ActiveRecord::Schema.define(version: 2019_04_27_103659) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "phone_number", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_04_26_044836) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "customer_id"
+    t.string "card_id"
     t.index ["user_id"], name: "index_credit_cards_on_user_id"
   end
 
