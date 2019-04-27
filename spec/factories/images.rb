51d8b6a9f16c3889e82347974decbde7280d85e8
reikoyanagi <pxfpd899>
2019-04-27 12:45:37 +0900

    items_controller_test

---
+++ spec/factories/images.rb
@@ -0,0 +1,8 @@
+
+FactoryBot.define do
+
+  factory :image do
+    item_image                 {"hoge.png"}
+    item
+  end
+end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,10 +2,12 @@ FactoryBot.define do
 
   factory :item do
     name                  {"アイテム"}
-    condition             {"1"}
+    condition             {"新品、未使用"}
     price                 {"1000"}
-    detail                {"新品未使用"}
+    detail                {"アイテムです"}
     status_id             {"1"}
+    created_at            { Faker::Time.between(2.days.ago, Time.now, :all) }
+    user
   end
 
 end
