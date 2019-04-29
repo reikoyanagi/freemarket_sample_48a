9bf61b64a4048f4b92c6e933d7a36490f6a57bff
smoto-shei <smoto_shei@yahoo.co.jp>
2019-04-29 14:17:57 +0900

    Fix delete preview for sell items
    削除ボタンを押しても削除されないエラーを修正した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,7 @@ class ItemsController < ApplicationController
     # 画像以外のファイルを格納
     @item = Item.new(item_params)
 
+    # transaction使うべき
     if image_judge != 0
       if @item.save &&
         # 画像ファイルは @item.id が必要なので @itemが保存された後に引っ張る
