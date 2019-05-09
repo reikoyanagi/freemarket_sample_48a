d76a482bff6fd822e162f975d7981b072c3f6a1b
reikoyanagi <pxfpd899>
2019-05-09 18:35:18 +0900

    Fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -44,11 +44,11 @@ set :unicorn_rack_env, "production"
 #
 # Global options
 # --------------
- set :ssh_options, {
-   keys: %w(/home/rlisowski/.ssh/id_rsa),
-   forward_agent: false,
-   auth_methods: %w(password)
- }
+ # set :ssh_options, {
+ #   keys: %w(/home/rlisowski/.ssh/id_rsa),
+ #   forward_agent: false,
+ #   auth_methods: %w(password)
+ # }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
